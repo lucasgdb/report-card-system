91e1c85c83b81294d0d15ef9990840f710c0ec4c
lucasgdb <lucasgdbittencourt@gmail.com>
2022-04-05 14:06:59 -0300

    chore: remove config

--- packages/server/seeds/01_user.ts
+++ packages/server/seeds/01_user.ts
@@ -1,18 +1,10 @@
 import { Knex } from 'knex';
-import * as bcrypt from 'bcryptjs';
 
 export const seed = async (knex: Knex) => {
   await knex('login').del();
+  await knex('student').del();
+  await knex('admin').del();
   await knex('user').del();
 
-  const encryptedPassword = bcrypt.hashSync('123', bcrypt.genSaltSync());
-
-  await knex('user').insert([
-    {
-      email: 'lucasgdbittencourt@gmail.com',
-      password: encryptedPassword,
-      name: 'Lucas',
-      lastname: 'Bittencourt',
-    },
-  ]);
+  await knex('user').insert({});
 };
--- packages/server/seeds/02_config.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from 'knex';
-
-export const seed = async (knex: Knex) => {
-  await knex('config').del();
-
-  await knex('config').insert([{ name: 'maximum_number_of_valid_logins', value: '2', public: true }]);
-};
