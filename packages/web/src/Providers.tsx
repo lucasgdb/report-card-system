e27f7a2531238e24c71a59f18f1631317dee590b
lucasgdb <lucasgdbittencourt@gmail.com>
2022-04-10 15:11:55 -0300

    refactor: change default snackbar position

--- packages/web/src/Providers.tsx
+++ packages/web/src/Providers.tsx
@@ -64,7 +64,7 @@ export default function Providers({ children }: ProvidersProps) {
     <RelayEnvironmentProvider environment={environment}>
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBRLocale}>
-          <Notification.SnackbarProvider anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+          <Notification.SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
             {children}
           </Notification.SnackbarProvider>
         </LocalizationProvider>
