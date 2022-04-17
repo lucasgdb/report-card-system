745c7db1c3ca9364885e4b8d01f0f891addda7ed
lucasgdb <lucasgdbittencourt@gmail.com>
2022-04-16 21:21:26 -0300

    refactor: improve code

---
+++ apps/web/src/components/Home/Profile/Information.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { graphql, useFragment } from 'relay-hooks';
+
+import { Information_student$key } from './__generated__/Information_student.graphql';
+
+const fragment = graphql`
+  fragment Information_student on Student {
+    RM
+    fullname
+  }
+`;
+
+const OuterProfileInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const RMText = styled.p`
+  font: normal normal 700 32px/40px Lexend;
+  color: #fafafa;
+
+  margin: 0;
+`;
+
+const FullnameText = styled.p`
+  font: normal normal 700 32px/40px Lexend;
+  color: #fafafa;
+
+  text-transform: uppercase;
+  margin: 0;
+`;
+
+const Status = styled.div`
+  padding: 11px 0;
+  width: 250px;
+
+  background-color: #22e675;
+  border-radius: 4px;
+`;
+
+const StatusText = styled.p`
+  font: normal normal 700 24px/30px Lexend;
+  color: #fafafa;
+  margin: 0;
+  text-align: center;
+
+  text-transform: uppercase;
+`;
+
+type InformationProps = {
+  student: Information_student$key;
+};
+
+export default function Information({ student }: InformationProps) {
+  const data = useFragment<Information_student$key>(fragment, student);
+
+  return (
+    <OuterProfileInformation>
+      <RMText>RM: {data.RM}</RMText>
+      <FullnameText>NOME: {data.fullname}</FullnameText>
+      <Status>
+        <StatusText>APROVADO</StatusText>
+      </Status>
+    </OuterProfileInformation>
+  );
+}
--- apps/web/src/components/Home/Profile/Profile.tsx
+++ apps/web/src/components/Home/Profile/Profile.tsx
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
 import { graphql, useFragment } from 'relay-hooks';
-import IconButton from '@mui/material/IconButton';
-import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 
 import { Profile_student$key } from './__generated__/Profile_student.graphql';
+import UploadPhotoButton from './UploadPhotoButton';
+import Information from './Information';
 
 const fragment = graphql`
   fragment Profile_student on Student {
-    RM
-    fullname
+    ...Information_student
   }
 `;
 
@@ -17,7 +16,6 @@ const OuterProfile = styled.div`
   height: 375px;
 
   background: linear-gradient(92.84deg, #011461 -8.18%, #0020a2 47.5%, #00d4ff 106.78%);
-  border-radius: 0 0 36px 36px;
 
   display: flex;
   align-items: center;
@@ -62,93 +60,15 @@ const ImageCircle = styled.div`
   border: 2px solid #ee7844;
 `;
 
-const CameraCircle = styled(IconButton)`
-  && {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-
-    width: 48px;
-    height: 48px;
-    border-radius: 50%;
-    background-color: #ee7844;
-
-    :hover {
-      background-color: #ee7844;
-    }
-  }
-`;
-
-const StyledCameraAltOutlinedIcon = styled(CameraAltOutlinedIcon)`
-  && {
-    color: #fff;
-  }
-`;
-
 const StudentImage = () => {
   return (
     <OuterStudentImage>
       <HomeIconsImage src="/assets/images/home_icons.svg" />
 
       <ImageCircle>
-        <CameraCircle color="primary">
-          <StyledCameraAltOutlinedIcon />
-        </CameraCircle>
+        <UploadPhotoButton />
       </ImageCircle>
     </OuterStudentImage>
-  );
-};
-
-const OuterProfileInformation = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 16px;
-`;
-
-const RMText = styled.p`
-  font: normal normal 700 32px/40px Lexend;
-  color: #fafafa;
-
-  margin: 0;
-`;
-
-const FullnameText = styled.p`
-  font: normal normal 700 32px/40px Lexend;
-  color: #fafafa;
-
-  text-transform: uppercase;
-  margin: 0;
-`;
-
-const Status = styled.div`
-  padding: 11px 0;
-  width: 250px;
-
-  background-color: #22e675;
-  border-radius: 4px;
-`;
-
-const StatusText = styled.p`
-  font: normal normal 700 24px/30px Lexend;
-  color: #fafafa;
-  margin: 0;
-  text-align: center;
-
-  text-transform: uppercase;
-`;
-
-const ProfileInformation = ({ student }: ProfileProps) => {
-  const data = useFragment<Profile_student$key>(fragment, student);
-
-  return (
-    <OuterProfileInformation>
-      <RMText>RM: {data.RM}</RMText>
-      <FullnameText>NOME: {data.fullname}</FullnameText>
-      <Status>
-        <StatusText>APROVADO</StatusText>
-      </Status>
-    </OuterProfileInformation>
   );
 };
 
@@ -157,11 +77,13 @@ type ProfileProps = {
 };
 
 export default function Profile({ student }: ProfileProps) {
+  const data = useFragment<Profile_student$key>(fragment, student);
+
   return (
     <OuterProfile>
       <StudentImage />
 
-      <ProfileInformation student={student} />
+      <Information student={data} />
 
       <ZLogoImage src="/assets/images/z_logo.svg" />
     </OuterProfile>
